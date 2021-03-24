8e2a1981289d20b33c1842f5bc5c89db3a01dd9d
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-03-24 12:02:13 -0300

    feat(project-structure): Sending submission form to /process and /subprocess (POST)

--- src/components/CreateProcessForm/CreateProcessForm.js
+++ src/components/CreateProcessForm/CreateProcessForm.js
@@ -24,16 +24,48 @@ async function getOptions(){
 
 function CreateProcessForm() {
   
+  const handleSubmit = event => {
+    const subprocess = valueTeste.map(d => ({
+      "subprocess_id" : d.value
+    }))
+    //tags.map(v => console.log(v.tag))
+    
+    var name = event.target.name.value
+
+    event.preventDefault();
+
+    var axios = require('axios');
+    var data = JSON.stringify({"name":name, "subprocess":subprocess});
+    alert(data);
+    var config = {
+      method: 'post',
+      url: 'https://lzsxdvovw7.execute-api.us-east-1.amazonaws.com/dev/process',
+      headers: { 
+        'Authorization': 'Bearer eyJraWQiOiJzbmEyYmFlNGtMbnhpODdPOU5nRDlkd3NzOWRJN2lPNExzQUd3c0lCa1cwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1NWMyMzQ2NC1mZWIxLTRkOWItYmViOC0yYzQ3YmEwMTFkNDQiLCJjb2duaXRvOmdyb3VwcyI6WyJsdWNraWUtdGVjaCJdLCJldmVudF9pZCI6IjI1MGM5OThhLTIwYTMtNDc0Zi1hNGNmLThjNjQ4N2ZiYThmYiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTM3NjcxMTIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX1hwUGtubkpzciIsImV4cCI6MTYxMzc3MDcxMiwiaWF0IjoxNjEzNzY3MTEyLCJqdGkiOiJiMGVjOWE4OC1hZmEzLTQ1MTQtOWEyYS04NjUxYTM5NmZhY2MiLCJjbGllbnRfaWQiOiI3MTNqcGgwZWxnc291ZTltY211M2xnY2o1aSIsInVzZXJuYW1lIjoidGhpYWdvLm0ifQ.LNtqXXWxyIDkQEKktocWTNj-raVOVsbK71m8J4_Ijy06_eD9CzYxqa4S7rhRaew-6Ql5NPH4gIDOfUHtkd42ttCZOuiphk8AtetpLuWuwB_M-3QWo68YWevW-X1hgn_fx9AXt9B8kz2FDbN6g2gylQ8jrEPdaB-tnnR5PkcXTZB7pKLz8moX_qB2Sfe8OTMey6abV1nEzrQ9CqPWPMklrR_CsEa2G4OZZdRO9orPRa9E5tmxXdaSH7hzR6cH6lSe4yb3ACJ-AxLB-ZsUpbkV0oLNmP3sDk3gB_uWO2TYpuZ-7FFqUYqZRzeYW1-SYvL7WCPQJHxQMDXzCA_sG4eqeA', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  }
+
   const handleChangeCombo = (e) => {
     var a = {value:e}
     var b = a.value
-    //alert(b)
     valueTeste = b
   }
 
   return(
     <Container maxWidth="sm">
-        <form>
+        <form onSubmit={handleSubmit}>
           <Grid container direction="column" justify="space-between" alignItems="flex-start">
             <Grid  container item xs={12} alignItems="flex-start" justify="center">
               <Typography color="primary" variant="h6" align="center" component="h2"  alignItems="center" justify="center">
@@ -72,13 +104,14 @@ function CreateProcessForm() {
             </Grid>
             <br/>
             <Grid container xs={12} sm={12}>
-                <Button
-                disableElevation
-                variant="contained"
-                color="primary"
-                fullWidth
-                endIcon={<SendIcon/>}
-                >
+              <Button
+              type="submit"
+              disableElevation
+              variant="contained"
+              color="primary"
+              fullWidth
+              endIcon={<SendIcon/>}
+              >
                 Cadastrar
             </Button>
 
--- src/components/CreateSubprocessForm/CreateSubprocessForm.js
+++ src/components/CreateSubprocessForm/CreateSubprocessForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -7,54 +7,98 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
 
+const formReducer = (state, event) => {
+  return {
+   ...state,
+   [event.name]: event.value
+ }
+}
 
 function CreateSubprocessForm() {
 
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit = event => {
+    var name = event.target.name.value
+    var limit_time = event.target.limit_time.value
+
+    event.preventDefault();//stop to reload the page
+    var axios = require('axios');
+    var data = JSON.stringify({"name":name,"limit_time":limit_time});
+    
+    var config = {
+      method: 'post',
+      url: 'https://lzsxdvovw7.execute-api.us-east-1.amazonaws.com/dev/subprocess',
+      headers: { 
+        'Authorization': 'Bearer eyJraWQiOiJzbmEyYmFlNGtMbnhpODdPOU5nRDlkd3NzOWRJN2lPNExzQUd3c0lCa1cwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1NWMyMzQ2NC1mZWIxLTRkOWItYmViOC0yYzQ3YmEwMTFkNDQiLCJjb2duaXRvOmdyb3VwcyI6WyJsdWNraWUtdGVjaCJdLCJldmVudF9pZCI6IjI1MGM5OThhLTIwYTMtNDc0Zi1hNGNmLThjNjQ4N2ZiYThmYiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTM3NjcxMTIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX1hwUGtubkpzciIsImV4cCI6MTYxMzc3MDcxMiwiaWF0IjoxNjEzNzY3MTEyLCJqdGkiOiJiMGVjOWE4OC1hZmEzLTQ1MTQtOWEyYS04NjUxYTM5NmZhY2MiLCJjbGllbnRfaWQiOiI3MTNqcGgwZWxnc291ZTltY211M2xnY2o1aSIsInVzZXJuYW1lIjoidGhpYWdvLm0ifQ.LNtqXXWxyIDkQEKktocWTNj-raVOVsbK71m8J4_Ijy06_eD9CzYxqa4S7rhRaew-6Ql5NPH4gIDOfUHtkd42ttCZOuiphk8AtetpLuWuwB_M-3QWo68YWevW-X1hgn_fx9AXt9B8kz2FDbN6g2gylQ8jrEPdaB-tnnR5PkcXTZB7pKLz8moX_qB2Sfe8OTMey6abV1nEzrQ9CqPWPMklrR_CsEa2G4OZZdRO9orPRa9E5tmxXdaSH7hzR6cH6lSe4yb3ACJ-AxLB-ZsUpbkV0oLNmP3sDk3gB_uWO2TYpuZ-7FFqUYqZRzeYW1-SYvL7WCPQJHxQMDXzCA_sG4eqeA', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      alert('Subprocesso cadastrado com sucesso')
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      alert('Erro ao cadastrar subprocesso')
+      console.log(error);
+    });
+    
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
   return(
     <Container maxWidth="sm">
-        <form>
-          <Grid container direction="column" justify="space-between" alignItems="flex-start">
-            <Grid  container item xs={12} alignItems="flex-start" justify="center">
-              <Typography color="primary" variant="h6" align="center" component="h2"  alignItems="center" justify="center">
-                <Box fontWeight="fontWeightBold">
-                CADASTRAR SUBPROCESSO
-                </Box>
-              </Typography>            
-            </Grid>
-            <br/>
-            <br/>
-            <Grid container xs={12}>
-                <Typography color="primary" variant="h6" component="h2" >
-                    Identificação
-                    <hr/>
-                </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container direction="column" justify="space-between" alignItems="flex-start">
+          <Grid  container item xs={12} alignItems="flex-start" justify="center">
+            <Typography color="primary" variant="h6" align="center" component="h2"  alignItems="center" justify="center">
+              <Box fontWeight="fontWeightBold">
+              CADASTRAR SUBPROCESSO
+              </Box>
+            </Typography>            
+          </Grid>
+          <br/>
+          <br/>
+          <Grid container xs={12}>
+              <Typography color="primary" variant="h6" component="h2" >
+                  Identificação
+                  <hr/>
+              </Typography>
 
-            </Grid>
-            <br/>
-            <Grid container spacing={4}>
-                <Grid item xs={12} sm={6}>
-                    <TextField size="small" fullWidth variant="outlined"  name="name" label="Nome"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField size="small" fullWidth variant="outlined"  name="name" label="Tempo Limite"/>
-                </Grid>
-            </Grid>
-            <br/>
-            <Grid container xs={12} sm={12}>
-                <Button
-                disableElevation
-                variant="contained"
-                color="primary"
-                fullWidth
-                endIcon={<SendIcon/>}
-                >
-                Cadastrar
-            </Button>
-
-            </Grid>
-            
+          </Grid>
+          <br/>
+          <Grid container spacing={4}>
+              <Grid item xs={12} sm={6}>
+                  <TextField size="small" fullWidth variant="outlined"  name="name" label="Nome"  onChange={handleChange}/>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                  <TextField size="small" fullWidth variant="outlined"  name="limit_time" label="Tempo Limite"  onChange={handleChange}/>
+              </Grid>
+          </Grid>
+          <br/>
+          <Grid container xs={12} sm={12}>
+              <Button
+              type="submit"
+              disableElevation
+              variant="contained"
+              color="primary"
+              fullWidth
+              endIcon={<SendIcon/>}
+              >
+              Cadastrar
+              </Button>
           </Grid>
           
+        </Grid> 
       </form>        
     </Container>
     )
