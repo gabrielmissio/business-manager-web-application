cdf5e5526737ed94811bca66647311384b684da3
gabrielmissio <gabmissil@gmail.com>
2021-04-09 21:43:08 -0300

    feat(ListRegisterView): Created funtional ListRegisterView, just need add the id parameter

--- src/components/ListRegisterView/ListRegisterView.js
+++ src/components/ListRegisterView/ListRegisterView.js
@@ -1,111 +1,25 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
+import React, {Component} from 'react';
+import Registers from './components/Registers';
 
+class ListRegisterView extends Component {
+    render() {
+        return (
+            <Registers registers={this.state.registers} />
+        )
+    } 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: '36ch',
-    backgroundColor: theme.palette.background.paper,
-  },
-  inline: {
-    display: 'inline',
-  },
-}));
+    state = {
+        registers: []
+    };
 
-export default function ListRegisterView() {
-
-  return (
-    <List >
-      <ListItem alignItems="flex-start">
-        <Grid container direction="column" justify="space-between" alignItems="flex-start">
-          <ListItemText
-            primary="Comentario ao encaminhar:"
-            secondary={
-              <React.Fragment>
-                {"gmissio | 15/02/2021 15:30"}
-              </React.Fragment>
-            }
-          />
-          <ListItemText
-            secondary={
-              <React.Fragment>
-                {"Encaminhado para Troca de tela | Encomenda de peça"}
-              </React.Fragment>
-            }
-          />
-        </Grid>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <Grid container direction="column" justify="space-between" alignItems="flex-start">
-          <ListItemText
-            primary="Adicionado comentario::"
-            secondary={
-              <React.Fragment>
-                {"gmissio | 15/02/2021 15:29"}
-              </React.Fragment>
-            }
-          />
-          <ListItemText
-            primary={
-              <React.Fragment>
-                {"Cliente aceitou o orçamento"}
-              </React.Fragment>
-            }
-          />
-        </Grid>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <Grid container direction="column" justify="space-between" alignItems="flex-start">
-          <ListItemText
-            primary="Adicionado comentario:"
-            secondary={
-              <React.Fragment>
-                {"gmissio | 15/02/2021 13:45"}
-              </React.Fragment>
-            }
-          />
-          <ListItemText
-            primary={
-              <React.Fragment>
-                {"Entrado em contato com o cliente e passado o orçamento, aguardando retorno"}
-              </React.Fragment>
-            }
-          />
-        </Grid>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <Grid container direction="column" justify="space-between" alignItems="flex-start">
-          <ListItemText
-            primary="Comentario ao encaminhar:"
-            secondary={
-              <React.Fragment>
-                {"gmissio | 15/02/2021 13:35"}
-              </React.Fragment>
-            }
-          />
-          <ListItemText
-            secondary={
-              <React.Fragment>
-                {"Aberta O.S. em Troca de tela | Orçamento"}
-              </React.Fragment>
-            }
-          />
-
-        </Grid>
-      </ListItem>
-      <Divider variant="inset" component="li" />
-    </List>
-  );
+    componentDidMount() {
+        fetch('https://ii9ik5bym6.execute-api.us-east-1.amazonaws.com/dev/service-order/6/register')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ registers: data })
+            })
+            .catch(console.log)
+    }
 }
+
+export default ListRegisterView;
