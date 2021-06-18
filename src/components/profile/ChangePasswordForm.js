dbb885fd433274ae7016c917d489693dbf5c1fa9
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-06-18 13:33:29 -0300

    feat(user): implemented method to change the user's password

--- src/components/profile/ChangePasswordForm.js
+++ src/components/profile/ChangePasswordForm.js
@@ -5,27 +5,42 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
+import { Auth } from 'aws-amplify';
 
-
+function changePassword(oldPassword, newPassword) {
+  Auth.currentAuthenticatedUser()
+    .then(user => {
+        return Auth.changePassword(user, oldPassword, newPassword);
+    })
+    .then(function(data) {
+      console.log(data)
+      alert('Senha atualizada com sucesso!')
+    })
+    .catch(function(err) {
+      console.log(err)
+      alert('Erro ao atualizar senha! Verifique o log do console para mais informações')
+   })
+}
 
 class ChangePasswordForm extends React.Component {
   
-  state = {
-    service_order: []
-  };
-
-  componentDidMount () {
-    
-  }
-
   render() {
     
     const handleSubmit = event => {          
    
-    event.preventDefault();
+      event.preventDefault();
+      let oldPassword = event.target.oldPassword.value
+      let newPassword = event.target.newPassword.value
+      let newPasswordConfirmation = event.target.newPasswordConfirmation.value
 
-    alert('nao disponivel')
-    
+      if (newPassword === newPasswordConfirmation){
+        changePassword(oldPassword, newPassword)
+      }else{
+        alert('Erro: a senha inserida no campo `Nova senha` precisa ser igual no campo `Nova senha confirmação`')  
+      }
+
+      
+      
     }    
   return(
     <Container maxWidth="sm">
@@ -40,13 +55,13 @@ class ChangePasswordForm extends React.Component {
           <br/>
           <Grid container spacing={4}>
             <Grid item xs={12} sm={6}>
-                <TextField type="password" size="small" fullWidth variant="outlined"  name="current_password" label="Senha atual"/>
+                <TextField type="password" size="small" fullWidth variant="outlined"  name="oldPassword" label="Senha atual"/>
             </Grid>
             <Grid item xs={12} sm={6}> 
-                <TextField type="password" size="small" fullWidth variant="outlined"  name="new_password" label="Nova senha"/>
+                <TextField type="password" size="small" fullWidth variant="outlined"  name="newPassword" label="Nova senha"/>
             </Grid>
             <Grid item xs={12} sm={6}> 
-                <TextField type="password" size="small" fullWidth variant="outlined"  name="new_password_confirmation" label="Nova senha confirmação"/>
+                <TextField type="password" size="small" fullWidth variant="outlined"  name="newPasswordConfirmation" label="Nova senha confirmação"/>
             </Grid>
           </Grid>
           <br/>
